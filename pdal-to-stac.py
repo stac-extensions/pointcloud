104ee266339f9ed9bd5974dc40388425d9e3fca8
kylemann16 <kyle@hobu.co>
2023-09-27 10:33:58 -0500

    Use pdal python library and static copc filepath to construct sample stac data.'

--- pdal-to-stac.py
+++ pdal-to-stac.py
@@ -1,29 +1,39 @@
 #!/usr/bin/env python
 
-# Read 'pdal info --all' output and emit a STAC pc object
-# conda create -n pdal-stac -c conda-forge pdal
-# conda activate pdal-stac
-# pdal info --all ~/dev/git/pdal/test/data/autzen/autzen-full.laz |./pdal-to-stac.py
+# conda install -c conda-forge pdal gdal
 
 import sys
 import json
 import os
+import pdal
 
+filename = "https://github.com/PDAL/data/raw/master/autzen/autzen-classified.copc.laz"
 
-data = sys.stdin.read()
+# pdal info --all call references hexbin, stats, and info filters
+r = pdal.Reader.copc(filename)
+hb = pdal.Filter.hexbin()
+s = pdal.Filter.stats()
+i = pdal.Filter.info()
 
-j = json.loads(data)
+pipeline: pdal.Pipeline = r | hb | s | i
+
+count = pipeline.execute()
+
+boundary = pipeline.metadata['metadata'][hb.type]
+stats = pipeline.metadata['metadata'][s.type]
+info = pipeline.metadata['metadata'][i.type]
+copc = pipeline.metadata['metadata'][r.type]
 
 def capture_date(pdalinfo):
     import datetime
-    year = pdalinfo['metadata']['creation_year']
-    day = pdalinfo['metadata']['creation_doy']
-    date = datetime.datetime(int(year), 1, 1) + datetime.timedelta(int(day) - 1)
-    return date.astimezone().isoformat()+'Z'
+    year = pdalinfo['creation_year']
+    day = pdalinfo['creation_doy']
+    date = datetime.datetime(int(year), 1, 1) + datetime.timedelta(int(day) - 1 if int(day) > 1 else int(day))
+    return date.isoformat()+'Z'
 
 def convertGeometry(geom, srs):
-    import ogr
-    import osr
+    from osgeo import ogr
+    from osgeo import osr
     in_ref = osr.SpatialReference()
     in_ref.SetFromUserInput(srs)
     out_ref = osr.SpatialReference()
@@ -49,37 +59,41 @@ def convertBBox(obj):
 output = {}
 
 try:
-    output['geometry'] = convertGeometry(j['boundary']['boundary_json'],j['metadata']['comp_spatialreference'])
+    output['geometry'] = convertGeometry(
+        boundary['boundary_json'],
+        copc['comp_spatialreference']
+    )
 except KeyError:
-    output['geometry'] = j['stats']['bbox']['EPSG:4326']['boundary']
+    output['geometry'] = stats['bbox']['EPSG:4326']['boundary']
 
-output['bbox'] = convertBBox(j['stats']['bbox']['EPSG:4326']['bbox'])
-output['id'] = os.path.basename(j['filename'])
+output['bbox'] = convertBBox(stats['bbox']['EPSG:4326']['bbox'])
+output['id'] = os.path.basename(filename)
 output['type'] = 'Feature'
 
-assets = {}
+assets = {'data': {'href': filename}}
 #assets['thumbnail'] =
 properties = {}
 
-properties['pc:schemas'] = j['schema']['dimensions']
-properties['pc:statistics'] = j['stats']['statistic']
+properties['pc:schemas'] = info['schema']['dimensions']
+properties['pc:statistics'] = stats['statistic']
 properties['title'] = "USGS 3DEP LiDAR"
 properties['item:provider'] = "USGS"
 properties['item:license'] = 'LICENSE'
 properties['pc:type'] = 'lidar' # eopc, lidar, radar, sonar
 try:
-    properties['pc:density'] = j['boundary']['avg_pt_per_sq_unit']
+    properties['pc:density'] = boundary['avg_pt_per_sq_unit']
 except KeyError:
     properties['pc:density'] = 0
-properties['pc:count'] = j['metadata']['count']
+properties['pc:count'] = count
 
-properties['datetime'] = capture_date(j)
+properties['datetime'] = capture_date(copc)
 
 output['properties'] = properties
 output['assets'] = assets
 
-link = {'rel':'self',"href":j['filename']}
+link = {'rel':'self',"href":filename}
 output['links'] = [link]
 
-sys.stdout.write(json.dumps(output,sort_keys=True,
-                  indent=2, separators=(',', ': ')))
+with open('examples/example-autzen.json', 'w') as autzen_out:
+    autzen_out.write(json.dumps(output, sort_keys=True, indent=2,
+                                separators=(',', ': ')))
